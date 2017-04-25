ab4d4fd756e1162884433d263f4d685a042670b7
Li Chaoyi <icylydia@gmail.com>
2017-04-25 16:51:33 +0900

    Add tab bar

--- ViewControllers/Main/Main.swift
+++ ViewControllers/Main/Main.swift
@@ -19,19 +19,28 @@ class NavigationVC: UINavigationController {
 }
 
 class TabVC: UITabBarController {
+    let localizeFileName = "Main"
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let homepageVC = HomepageVC()
+        homepageVC.tabBarItem.image = #imageLiteral(resourceName: "TabbarPlan")
+        homepageVC.tabBarItem.title = NSLocalizedString("TabbarPlan", tableName: localizeFileName, comment: "")
         let homepageNavi = NavigationVC(rootViewController: homepageVC)
 
         let attractionVC = AttractionVC()
+        attractionVC.tabBarItem.image = #imageLiteral(resourceName: "TabbarAttraction")
+        attractionVC.tabBarItem.title = NSLocalizedString("TabbarAttraction", tableName: localizeFileName, comment: "")
         let attractionNavi = NavigationVC(rootViewController: attractionVC)
 
         let mapVC = MapVC()
+        mapVC.tabBarItem.image = #imageLiteral(resourceName: "TabbarMap")
+        mapVC.tabBarItem.title = NSLocalizedString("TabbarMap", tableName: localizeFileName, comment: "")
         let mapNavi = NavigationVC(rootViewController: mapVC)
 
         let settingVC = SettingVC()
+        settingVC.tabBarItem.image = #imageLiteral(resourceName: "TabbarSetting")
+        settingVC.tabBarItem.title = NSLocalizedString("TabbarSetting", tableName: localizeFileName, comment: "")
         let settingNavi = NavigationVC(rootViewController: settingVC)
 
         let vcs = [homepageNavi, attractionNavi, mapNavi, settingNavi]
